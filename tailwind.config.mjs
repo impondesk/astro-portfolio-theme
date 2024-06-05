fdcacdcfde89b4ef512091c409edf4075eccff47
Israni, Murli <murli.p.israni@gmail.com>
2024-06-05 14:02:29 +0530

    misc

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,12 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 
+import { fontFamily } from "tailwindcss/defaultTheme";
+
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  corePlugins: {
+    aspectRatio: false,
+    borderOpacity: false,
+    fontVariantNumeric: false,
+    ringOffsetColor: false,
+    ringOffsetWidth: false,
+    scrollSnapType: false,
+    textOpacity: false,
+    touchAction: false,
+  },
+  darkMode: ["class", '[data-theme="dark"]'],
   theme: {
     fontFamily: {
-      body: ['"Open Sans"'],
+      body: ["Poppins", '"Open Sans"'],
     },
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require("@tailwindcss/aspect-ratio"),
+    require("@tailwindcss/typography"),
+  ],
 };
